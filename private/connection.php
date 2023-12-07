f04bcd939f31002913e5ebbe7150252a6bec7258
Sahif <sahif.student@peninsulamalaysia.edu.my>
2023-12-08 03:24:09 +0800

    E-Woodball Sports

--- private/connection.php
+++ private/connection.php
@@ -1,13 +1,11 @@
 <?php
-	$dbhost = "localhost";
-	$dbuser = "u478659771_ldwps";
-	$dbpass = "PSwoodball2022";
-	$dbname = "u478659771_nwps";
+$dbhost = "localhost";
+$dbuser = "u418262249_ewbs";
+$dbpass = "E-wbsports2024";
+$dbname = "u418262249_ewb";
 
-	$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-	if(!$con)
-	{
-		die("failed to connect");
-	}
-?>
+if (!$con) {
+	die("failed to connect");
+}
